484dfa1e6c78102a972b49ad415907baaab3375e
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-15 23:32:06 +0530

    login_view_created

--- profile_api/views.py
+++ profile_api/views.py
@@ -7,6 +7,9 @@ from profile_api import models
 from rest_framework.authentication import TokenAuthentication
 from profile_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 
 class HelloWorldApiView(APIView):
@@ -35,3 +38,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
