7deb936fd32ce480155a46aa8d6d927280bda9bc
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-15 21:31:45 +0530

    post_method_created

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,7 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profile_api.serializers import HelloSerializer
+
 
 class HelloWorldApiView(APIView):
 
-    def get(self,request,format=None):
+    serializer_class = HelloSerializer
+
+    def get(self, request, format=None):
         return Response("Hello World")
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message': message})
+        else:
+            return Response({'error': serializer.errors, 'error_message': serializer.error_messages},
+                            status=status.HTTP_400_BAD_REQUEST)
