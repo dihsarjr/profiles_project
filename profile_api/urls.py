8ade6ac8b037bfa6b000c2cb5836c08406ae8525
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-15 23:00:18 +0530

    url_connected

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from profile_api.views import HelloWorldApiView
+from django.urls import path, include
+from profile_api.views import HelloWorldApiView, UserProfileViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('profile', UserProfileViewSet)
+
 
 urlpatterns = [
-   
-    path('',HelloWorldApiView.as_view())
+    path('hello', HelloWorldApiView.as_view()),
+    path('', include(router.urls))
 ]
