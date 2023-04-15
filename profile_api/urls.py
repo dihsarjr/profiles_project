5d822d1828786798031a6e118a255165910eb481
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-15 23:43:27 +0530

    login_function_completed

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from profile_api.views import HelloWorldApiView, UserProfileViewSet
+from profile_api.views import HelloWorldApiView, UserProfileViewSet,UserLoginApiView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,5 +9,6 @@ router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
     path('hello', HelloWorldApiView.as_view()),
+    path('login/', UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
